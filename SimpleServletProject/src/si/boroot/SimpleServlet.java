eae29a508bd201c3c35e8e3c714645cb9911f28d
Boroot <Boroot>
2014-09-12 20:19:31 +0200

    version 1.1

--- SimpleServletProject/src/si/boroot/SimpleServlet.java
+++ SimpleServletProject/src/si/boroot/SimpleServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "A simple servlet", urlPatterns = { "/AdvancedServlet" })
+
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,5 +26,9 @@ public class SimpleServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("SimpleServlet sporoca"+ userName+ "je tvoj username");
 	}
 }
