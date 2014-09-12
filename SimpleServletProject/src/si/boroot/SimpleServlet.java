2518cc90aae3cd9e7d8976cdcb3317b525d8dbe7
Boroot <Boroot>
2014-09-12 21:31:51 +0200

    verzija 1.1.1

--- SimpleServletProject/src/si/boroot/SimpleServlet.java
+++ SimpleServletProject/src/si/boroot/SimpleServlet.java
@@ -3,15 +3,17 @@ package si.boroot;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SimpleServlet
  */
-
+// @WebServlet(description = "A simple servlet", urlPatterns = { "/AdvancedServlet" })
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -19,16 +21,24 @@ public class SimpleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello from GET method! ");
-		PrintWriter writer = response.getWriter();
-		writer.println("<h3> Hello in html</h3>");
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		PrintWriter ven = response.getWriter();
 		String userName = request.getParameter("userName");
-		out.println("SimpleServlet sporoca"+ userName+ "je tvoj username");
+		
+		//kreacija context objekta, kjer se objekt lahko prenaša med razliènimi brskalniki in uporabniki
+		ServletContext context =request.getServletContext();
+		
+		//kreacija session objekta "seja"
+		HttpSession seja = request.getSession();
+		
+		if (userName != "" && userName != null){
+		seja.setAttribute("savedUserName", userName);
+		context.setAttribute("savedUserName", userName);
+		}
+		
+		ven.println("Request parameter ima username " +userName);
+		ven.println("Session parameter ima username " +(String) seja.getAttribute("savedUserName"));
+		ven.println("Context parameter ima username "+(String) context.getAttribute("savedUserName"));
 	}
 }
